80d55aaf7e3ec61d36714ea483146263e3d1ebfd
Arnon Suksiri <arnon.suksili@gmail.com>
2020-11-16 16:08:24 +0700

    First query form db

--- src/Main/java/Main.java
+++ src/Main/java/Main.java
@@ -7,15 +7,21 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.sql.ResultSet;
+
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("/FXML/sample.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/FXML/login.fxml"));
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(root, 600, 400));
         primaryStage.show();
         DBConnection dbConnection = new DBConnection();
+        ResultSet rs = dbConnection.getAllRow("users");
+        while(rs.next()){
+            System.out.println(rs.getString("username") + " " + rs.getString("name"));
+        }
     }
 
 
--- src/Main/java/models/DBConnection.java
+++ src/Main/java/models/DBConnection.java
@@ -4,19 +4,31 @@ import java.sql.*;
 
 public class DBConnection {
 
+    private Connection connection;
+
     public DBConnection(){
         try {
             Class.forName("com.mysql.jdbc.Driver");
-            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/sawarehouse", "root", "");
-            Statement statement = connection.createStatement();
-            ResultSet rs = statement.executeQuery("SELECT * FROM categories");
-            while (rs.next()){
-                System.out.println(rs.getString("name"));
-            }
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/sawarehouse?useSSL=false", "root", "");
+//            Statement statement = connection.createStatement();
+//            ResultSet rs = statement.executeQuery("SELECT * FROM categories");
+//            while (rs.next()){
+//                System.out.println(rs.getString("name"));
+//            }
         }catch (ClassNotFoundException e){
             e.printStackTrace();
         }catch (SQLException e){
             e.printStackTrace();
         }
+    }
+
+    public ResultSet getAllRow(String table){
+        try {
+            Statement stmt = connection.createStatement();
+            return stmt.executeQuery("SELECT * FROM " + table);
+        } catch (SQLException throwables) {
+            System.out.println("error");
+        }
+        return null;
     }
 }
